871e6f7c2ccd051ed62f76c9ca9a1e56c0f563a4
naveenredddy123 <96879456+naveenredddy123@users.noreply.github.com>
2022-10-13 20:09:31 +0530

    Update HelloWorld.java
    commiit_1.3v

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,4 @@ public class HelloWorld {
     System.out.println("Hello world!");
     System.out.println("Hello world!");
     System.out.println("Hello world!");
-    System.out.println("Hello world!");
-    System.out.println("Hello world!");
-  }
 }
